4ea29afb9a79a2f457c5a6f4709c3f29bc0c3bfd
Nimish <nimishgupta18396@gmail.com>
2020-05-03 18:11:53 +0530

    feat: added the inquirer question prompts

--- src/questions.js
+++ src/questions.js
@@ -1,16 +1,39 @@
-const inquirer = require("inquirer");
+const inquirer = require('inquirer');
 
-//export const SearchPrompt = inquirer.createPromptModule();
+const getSearchPrompt = async () =>
+	inquirer.prompt([
+		{
+			type: 'input',
+			name: 'repoQuery',
+			message: 'Enter the repo to be searched:',
+			validate: (val) =>
+				val.length !== 0 ? true : `Searched repo couldn't be empty.`,
+		},
+	]);
 
+const getSelectRepoPrompt = async (repos) =>
+	inquirer.prompt({
+		type: 'list',
+		name: 'repo',
+		message:
+			'Please select the repo of which you want to have maintainers list:',
+		choices: repos.map((repo) => ({
+			name: repo.full_name,
+			value: repo,
+		})),
+		validate: (val) => (val.length !== 0 ? true : `Please select a repo.`),
+		pageSize: 10,
+	});
 
-const getSearchPrompt = async () => {
-    const answers = await inquirer.prompt([{
-        type: 'input',
-        name: 'repo_query',
-        message: 'Enter the repo to be searched:'
-    }]);
-    return answers;
+const repoDoesNotExistPrompt = async () =>
+	inquirer.prompt({
+		type: 'confirm',
+		name: 'searchAgain',
+		message: `Repo doesn't exist. Do you want to search again?`,
+	});
+
+module.exports = {
+	getSearchPrompt,
+	getSelectRepoPrompt,
+	repoDoesNotExistPrompt,
 };
-
-
-module.exports = { getSearchPrompt };
